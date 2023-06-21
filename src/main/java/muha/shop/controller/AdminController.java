03e7355ae837c636165cd1f3f8ffe7596b7bd076
AbayMuhanov <98691602+AbayMuhanov@users.noreply.github.com>
2023-06-21 18:12:48 +0600

    Version 0.4

---
+++ src/main/java/muha/shop/controller/AdminController.java
@@ -0,0 +1,29 @@
+package muha.shop.controller;
+
+import muha.shop.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @GetMapping("/feedbacks")
+    public String checkUserFeedbacks(Model model) {
+        model.addAttribute("feedbacks", feedbackService.findAllIsPublishedFalse());
+        return "admin_feedback";
+    }
+
+    @GetMapping("/feedback_post")
+    public String postFeedback(@RequestParam Long feedbackId) {
+        feedbackService.postFeedback(feedbackId);
+        return "redirect:/admin/feedbacks";
+    }
+}
